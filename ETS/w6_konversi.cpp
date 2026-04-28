d67bce4395f57914a6bfcc1c3fd32a8ca3ba6005
nitsabit <5025251023@student.its.ac.id>
2026-04-28 19:10:19 +0700

    Add files via upload

---
+++ ETS/w6_konversi.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <stack>
+#include <string>
+#include <cctype>
+using namespace std;
+
+int precedence(char op) {
+    if (op == '^')
+        return 3;
+    else if (op == '*' || op == '/')
+        return 2;
+    else if (op == '+' || op == '-')
+        return 1;
+    else
+        return 0;
+}
+
+bool isOperator(char c) {
+    return (c == '+' || c == '-' || c == '*' || c == '/' || c == '^');
+}
+
+string infixToPostfix(string infix) {
+    stack<char> st;
+    string postfix = "";
+
+    for (int i = 0; i < infix.length(); i++) {
+        char c = infix[i];
+
+        if (isalnum(c)) {
+            postfix += c;
+        }
+
+        else if (c == '(') {
+            st.push(c);
+        }
+
+        else if (c == ')') {
+            while (!st.empty() && st.top() != '(') {
+                postfix += st.top();
+                st.pop();
+            }
+            if (!st.empty())
+                st.pop(); 
+        }
+
+        else if (isOperator(c)) {
+            while (!st.empty() && precedence(st.top()) >= precedence(c)) {
+                postfix += st.top();
+                st.pop();
+            }
+            st.push(c);
+        }
+    }
+
+    while (!st.empty()) {
+        postfix += st.top();
+        st.pop();
+    }
+
+    return postfix;
+}
+
+int main() {
+    string infix;
+
+    cout << "Masukkan ekspresi infix: ";
+    cin >> infix;
+
+    string postfix = infixToPostfix(infix);
+
+    cout << "Postfix: " << postfix << endl;
+
+    return 0;
+}
